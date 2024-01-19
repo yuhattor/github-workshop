b2921a5fe46505c766aca5f39adbd27a244d05a8
yuhattor <yuhattor@users.noreply.github.com>
2024-01-19 11:11:33 +0000

    deploy: 04e46e4f95371e7070c0f32f25107a8ad8ea9312

---
+++ overwrite.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById("arrow-back").setAttribute("href", "https://github.github.com/copilot-workshop/");
+  document.getElementById("done").setAttribute("href", "https://github.github.com/copilot-workshop/");
+
+// hide element with id codelab-feedback after 5 seconds
+  document.getElementById("codelab-feedback").style.display = "none";
+
+});
